1f847ea2df7c12c5f6d848e38664a93ceb9c7763
David M Sibley <sibley@wisc.edu>
2018-08-03 12:30:37 -0500

    rollup more than one file

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,8 @@
 import { rollup } from 'rollup';
 import minify from 'rollup-plugin-minify-es';
 import resolve from 'rollup-plugin-node-resolve';
-
-let fileName = 'demo';
-let objName = 'Demo';
+import glob from 'glob';
+import path from 'path';
 
 let plugins = {
   full: [
@@ -19,42 +18,63 @@ let plugins = {
   ]
 };
 
+function configureFileBundle(fileName) {
+  return [
+    {
+      input: `src/${fileName}.js`,
+      plugins: plugins.full,
+      output: {
+        file: `dist/${fileName}.js`,
+        name: fileName,
+        format: 'iife'
+      }
+    },
+    {
+      input: `src/${fileName}.js`,
+      plugins: plugins.min,
+      output: {
+        file: `dist/${fileName}.min.js`,
+        name: fileName,
+        format: 'iife'
+      }
+    },
+    {
+      input: `src/${fileName}.js`,
+      plugins: plugins.full,
+      output: {
+        file: `dist/${fileName}.mjs`,
+        name: fileName,
+        format: 'es'
+      }
+    },
+    {
+      input: `src/${fileName}.js`,
+      plugins: plugins.min,
+      output: {
+        file: `dist/${fileName}.min.mjs`,
+        name: fileName,
+        format: 'es'
+      }
+    }
+  ];
+}
 
-export default [
-  {
-    input: `src/${fileName}.js`,
-    plugins: plugins.full,
-    output: {
-      file: `dist/${fileName}.js`,
-      name: objName,
-      format: 'iife'
-    }
-  },
-  {
-    input: `src/${fileName}.js`,
-    plugins: plugins.min,
-    output: {
-      file: `dist/${fileName}.min.js`,
-      name: objName,
-      format: 'iife'
-    }
-  },
-  {
-    input: `src/${fileName}.js`,
-    plugins: plugins.full,
-    output: {
-      file: `dist/${fileName}.mjs`,
-      name: objName,
-      format: 'es'
-    }
-  },
-  {
-    input: `src/${fileName}.js`,
-    plugins: plugins.min,
-    output: {
-      file: `dist/${fileName}.min.mjs`,
-      name: objName,
-      format: 'es'
-    }
-  },
-];
+async function createBundles(extension, locator) {
+  return new Promise(function(resolve, reject) {
+    glob(locator, null, function(er, files) {
+      if (!er && files) {
+        let result = files.reduce(function(prev, curr) {
+          let name = path.basename(curr, extension);
+          let bundle = configureFileBundle(name);
+          let result = prev.concat(bundle);
+          return result;
+        }, []);
+        resolve(result);
+      } else {
+        reject();
+      }
+    })
+  });
+}
+
+export default createBundles('.js', 'src/*.js');
---
+++ src/demo1.js
@@ -0,0 +1,6 @@
+
+import './lib/myuw-app-bar/demo1.js';
+import '@myuw-web-components/myuw-profile';
+import '@myuw-web-components/myuw-drawer';
+import '@myuw-web-components/myuw-app-styles';
+import '@myuw-web-components/myuw-help';
